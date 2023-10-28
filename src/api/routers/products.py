d9c573c180543db2af318e30c3be84138708371d
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-10-27 22:10:03 -0300

    feat: define response models and status codes for endpoints

--- src/api/routers/products.py
+++ src/api/routers/products.py
@@ -1,6 +1,6 @@
 from fastapi import APIRouter, Depends
 from dependencies import get_products_service
-from schemas.products import CreateProduct, UpdateProduct
+from schemas.products import CreateProduct, Product, UpdateProduct
 
 from service.products import ProductsService
 
@@ -8,24 +8,24 @@ from service.products import ProductsService
 products_router = APIRouter(prefix="/products", tags=["products"])
 
 
-@products_router.get("/")
+@products_router.get("/", response_model=list[Product])
 def find(service: ProductsService = Depends(get_products_service)):
     return service.find_all()
 
 
-@products_router.get("/{id}")
+@products_router.get("/{id}", response_model=Product)
 def find_one(id: int, service: ProductsService = Depends(get_products_service)):
     return service.find_one(id)
 
 
-@products_router.post("/")
+@products_router.post("/", response_model=Product, status_code=201)
 def create(
     product: CreateProduct, service: ProductsService = Depends(get_products_service)
 ):
     return service.create(product)
 
 
-@products_router.put("/{id}")
+@products_router.put("/{id}", response_model=Product)
 def update(
     id: int,
     product: UpdateProduct,
@@ -34,6 +34,6 @@ def update(
     return service.update(id, product)
 
 
-@products_router.delete("/{id}")
+@products_router.delete("/{id}", status_code=204)
 def delete(id: int, service: ProductsService = Depends(get_products_service)):
     return service.delete(id)
