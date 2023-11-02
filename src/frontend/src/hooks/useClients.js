ec3f024c9d4ccc36e9421532834855040162aca4
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-11-02 12:27:51 -0300

    refactor: declare useClients utility functions inside useEffect

--- src/frontend/src/hooks/useClients.js
+++ src/frontend/src/hooks/useClients.js
@@ -4,25 +4,24 @@ import { API_URL } from '../constants';
 export function useClients(clientId) {
   const [clients, setClients] = useState([]);
 
-  const getClients = async () => {
-    const response = await fetch(`${API_URL}/clients`);
-    const data = await response.json();
-    setClients(data);
-  };
-
-  const getSingleClient = async () => {
-    const response = await fetch(`${API_URL}/clients/${clientId}`);
-    const data = await response.json();
-    setClients([data]);
-  };
-
   useEffect(() => {
+    const getClients = async () => {
+      const response = await fetch(`${API_URL}/clients`);
+      const data = await response.json();
+      setClients(data);
+    };
+
+    const getSingleClient = async () => {
+      const response = await fetch(`${API_URL}/clients/${clientId}`);
+      const data = await response.json();
+      setClients([data]);
+    };
     if (!clientId) {
       getClients();
     } else {
       getSingleClient();
     }
-  }, []);
+  }, [clientId]);
 
   return {
     clients,
