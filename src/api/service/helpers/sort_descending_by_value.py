348d3d216709a240f829b148ebb78a6debaead8c
Raphael Oliveira <raphael.foliveira1@gmail.com>
2023-12-07 17:01:50 -0300

    refactor: rename param to dict_entries

--- src/api/service/helpers/sort_descending_by_value.py
+++ src/api/service/helpers/sort_descending_by_value.py
@@ -1,7 +1,7 @@
 from typing import ItemsView
 
 
-def sort_descending_by_value(tuples: ItemsView[str, int]) -> list[tuple[str, int]]:
-    tuples_list = list(tuples)
+def sort_descending_by_value(dict_entries: ItemsView[str, int]) -> list[tuple[str, int]]:
+    tuples_list = list(dict_entries)
     tuples_list.sort(key=lambda x: x[1], reverse=True)
     return tuples_list
